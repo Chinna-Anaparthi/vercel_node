805d4f33ba0fbcb402a1b769072511dcc67c29dc
Chinna-Anaparthi <123636616+Chinna-Anaparthi@users.noreply.github.com>
2023-10-17 18:45:03 +0530

    nine commit.

--- Database/RegularFieldWork.js
+++ Database/RegularFieldWork.js
@@ -28,6 +28,6 @@ const regularFieldWorkSchema = new mongoose.Schema({
   status: String,
 });
 
-const RegularFieldWork = mongoose.model('RegularFieldWork', regularFieldWorkSchema);
+const RegularFieldWork = mongoose.model('RegularField', regularFieldWorkSchema);
 
 module.exports = RegularFieldWork;
